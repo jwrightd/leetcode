b186c64ba052d822fbd0e29be0947445537d116e
jwrightd <jameswright112106@gmail.com>
2026-07-02 20:53:08 +0000

    Sync LeetCode submission Runtime - 899 ms (55.90%), Memory - 13 MB (38.28%)

---
+++ 0322-coin-change/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        # ok pure DP soln
+        # dp[i] is fewest number of coins needed to get sum of i
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
