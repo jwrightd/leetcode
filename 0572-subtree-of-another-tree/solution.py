c85343c7a1e1ffa1c8481306416040ef2f8d7314
jwrightd <jameswright112106@gmail.com>
2026-07-18 23:31:22 +0000

    Sync LeetCode submission Runtime - 56 ms (6.42%), Memory - 20.8 MB (5.03%)

--- 0572-subtree-of-another-tree/solution.py
+++ 0572-subtree-of-another-tree/solution.py
@@ -1,29 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        if root == None:
-            return root == subRoot
-        return self.recur(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        """
-        :type root: Optional[TreeNode]
-        :type subRoot: Optional[TreeNode]
-        :rtype: bool
-        """
-    def recur(self, root, subRoot):
-        if root == None and subRoot == None:
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # either root can be subtree, left can be St, or right can be St
+        def validNode(node, subtree):
+            if node == None and subtree == None:
+                return True
+            
+            if (node == None and subtree != None) or (node != None and subtree == None) or node.val != subtree.val:
+                return False
+            
+            
+            return validNode(node.left, subtree.left) and validNode(node.right, subtree.right)
+        
+        if validNode(root, subRoot):
+            return True 
+        if root.left and self.isSubtree(root.left, subRoot) or root.right and self.isSubtree(root.right, subRoot):
             return True
-        elif root == None or subRoot == None:
-            #print(root, subRoot)
-            return False
-        elif root.val != subRoot.val:
-            #print(root.val, subRoot.val)
-            return False
-        l = self.recur(root.left, subRoot.left)
-        r = self.recur(root.right, subRoot.right)
-        return l and r
+        return False
         
