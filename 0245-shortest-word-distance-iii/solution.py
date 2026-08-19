d19b85ec3f3afa03929fa15b27fd15d5355d01a5
jwrightd <jameswright112106@gmail.com>
2026-08-19 19:50:28 +0000

    Sync LeetCode submission Runtime - 21 ms (84.37%), Memory - 35.4 MB (60.92%)

--- 0245-shortest-word-distance-iii/solution.py
+++ 0245-shortest-word-distance-iii/solution.py
@@ -1,33 +1,42 @@
-class Solution(object):
-    def shortestWordDistance(self, wordsDict, word1, word2):
-        """
-        :type wordsDict: List[str]
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        # can have list of idx for w1, w2
-        # n2 soln is brute force
-        # can we do in nlogn if we sort both?
-        # most recent idxs
-        same = (word1 == word2)
-        w1 = -1
-        w2 = -1
-        idx = 0
-        shortestDist = float('inf')
-        n = len(wordsDict)
-        alternate = 1
-        while idx < n:
-            if wordsDict[idx] == word1 and alternate:
-                w1 = idx
-                if same:
-                    alternate = 0
-            elif wordsDict[idx] == word2:
-                w2 = idx
-                if same:
-                    alternate = 1
-            if w1 != -1 and w2 != -1:
-                shortestDist = min(shortestDist, abs(w2 - w1))
-            #print(w1, w2)
-            idx += 1
-        return shortestDist
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        # we want some list of indices for our two desired words
+        first = []
+        second = []
+        for idx, val in enumerate(wordsDict):
+            if val == word1:
+                first.append(idx)
+            elif val == word2:
+                second.append(idx)
+
+        shortest = float('inf')
+        # deal with same word1==word2 here:
+        if word1 == word2:
+            for idx in range(1, len(first)):
+                shortest = min(shortest, first[idx] - first[idx - 1])
+            return shortest
+            # just want to check every 2 subsequent idx
+
+
+        # now, algo
+        # we want the shortest distance, we also know that these two lists are sorted
+        # brute force is checking all pairs which is N2, but we can do in O(N) i think
+        # we have a runnning min, we check current min between first and second at ptrs
+        # if first[ptr] > second[ptr], we increment second_ptr
+        # otherwise first ptr
+
+        
+        first_ptr = 0
+        second_ptr = 0
+        M, N = len(first), len(second)
+
+        while first_ptr < M and second_ptr < N:
+            current_dist = abs(first[first_ptr] - second[second_ptr])
+            shortest = min(shortest, current_dist)
+            if first[first_ptr] > second[second_ptr]:
+                second_ptr += 1
+            else:
+                first_ptr += 1
+        return shortest
+
+        
